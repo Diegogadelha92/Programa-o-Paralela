3362b8f7f7814b72b177f7eb538351619779e792
PedroHenriqueMQ <pedro_q777@live.com>
2025-03-25 22:02:54 -0300

    Incluindo detecção de placas e otimizando a lógica do endpoint de upload

--- app/routes/main_routes.py
+++ app/routes/main_routes.py
@@ -1,9 +1,12 @@
 import os
+
 from flask import Blueprint, request, jsonify, render_template
 from werkzeug.utils import secure_filename
-from app.services.processamento import processar_imagem
 from flask import current_app as app
+from app.services.pre_processamento import processar_imagem
+from app.services.identificacao_placas import reconhecer_placa
 import json
+import numpy as np
 
 bp = Blueprint('main_routes', __name__, url_prefix="")
 
@@ -20,18 +23,26 @@ def upload_imagem():
     if arquivo.filename == '':
         return jsonify({'erro': 'Nome do arquivo inválido'}), 400
 
+    try:
+        imagem_bytes = arquivo.read()
+        imagem_np = np.frombuffer(imagem_bytes, dtype=np.uint8)
+    except Exception as e:
+        return jsonify({'erro': f'Erro ao processar imagem: {str(e)}'}), 400
+
     nome_arquivo = secure_filename(arquivo.filename)
     caminho_arquivo = os.path.join(app.config['UPLOAD_FOLDER'], nome_arquivo)
-    arquivo.save(caminho_arquivo)
 
-    resultados = processar_imagem(caminho_arquivo)
+    placa_detectada, error = reconhecer_placa(imagem_np, caminho_arquivo)
+    if error:
+        return jsonify({'erro': error}), 400
 
-    if resultados is None:
+    processamento_placa = processar_imagem(placa_detectada)
+    if processamento_placa is None:
         return jsonify({'erro': 'Arquivo não é uma imagem válida'}), 400
-    
+
     try:
-        json_resultado = json.dumps([tuple(item.tolist() for item in resultado) for resultado in resultados])
+        json_resultado = json.dumps([tuple(item.tolist() for item in resultado) for resultado in processamento_placa])
     except Exception as e:
         return jsonify({'erro': f'Erro ao converter resultado para JSON: {str(e)}'}), 500
 
-    return jsonify({'resultados': json_resultado})
+    return jsonify({'resultados': json_resultado, 'placas': [placa.tolist() for placa in placa_detectada]})
